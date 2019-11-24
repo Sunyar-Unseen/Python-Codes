3bc0ce9ddc35a5569f39fde1ab374560a2122c92
Yatendra <yatendra@sunyarunseenindia.com>
2019-11-24 17:52:19 +0530

    Assign2 completed

--- Learning/assign2.py
+++ Learning/assign2.py
@@ -1,10 +1,44 @@
-class BankAccount:
-    def __init__(self,amount):
-        self.amount = amount
-    def chk_balance(self):
-        print(f"Your current balance is: ", self.amount)
-    def deposit(self):
-        return 
-    
-p1 = BankAccount(700000)
-p1.chk_balance()
+
+# with both a deposit() and a withdraw() function 
+class BankAccount: 
+	def __init__(self): 
+		self.balance=0
+		print("Hello!!! Welcome to the Deposit & Withdrawal Machine") 
+
+	def deposit(self): 
+		amount=float(input("Enter amount to be Deposited: ")) 
+		self.balance += amount 
+		print("\n Amount Deposited:",amount) 
+
+	def withdraw(self): 
+		amount = float(input("Enter amount to be Withdrawn: ")) 
+		if self.balance>=amount: 
+			self.balance-=amount 
+			print("\n You Withdrew:", amount) 
+		else: 
+			print("\n Insufficient balance ") 
+
+	def display(self): 
+		print("\n Net Available Balance=",self.balance) 
+
+
+#Define a child class min balance
+
+class MinimumBalanceAccount(BankAccount):
+    def __init__(self,minimum_balance):
+        BankAccount.__init__(self)
+        self.minimum_balance = minimum_balance
+    def withdraw(self,amount):
+        if self.balance - amount < self.minimum_balance:
+            print("Sorry, minimum balance is low ")
+        else:
+            BankAccount.withdraw(self.amount)
+
+# creating an object of class 
+s = BankAccount() 
+
+# Calling functions with that class object 
+s.deposit() 
+s.withdraw() 
+s.display() 
+
